088b95ee4f265d6fc2acbd9dd27e632952f31363
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2022-03-20 14:16:33 +0530

    initialize jdbi, dao and register the resource in the application

--- src/main/java/org/example/DropwizardJDBIExampleApplication.java
+++ src/main/java/org/example/DropwizardJDBIExampleApplication.java
@@ -1,9 +1,16 @@
 package org.example;
 
 import io.dropwizard.Application;
+import io.dropwizard.jdbi3.JdbiFactory;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.example.api.Person;
+import org.example.db.IPersonDAO;
+import org.example.db.PersonDAO;
 import org.example.health.DropwizardJDBIExampleHealthCheck;
+import org.example.resources.PersonResource;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.mapper.immutables.JdbiImmutables;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,6 +29,12 @@ public class DropwizardJDBIExampleApplication extends Application<DropwizardJDBI
 
     public void run(DropwizardJDBIExampleConfiguration dropwizardJDBIExampleConfiguration, Environment environment) {
         LOGGER.info("Initializing server...");
+        JdbiFactory factory = new JdbiFactory();
+        Jdbi jdbi = factory.build(environment, dropwizardJDBIExampleConfiguration.getDatabase(), "h2");
+        jdbi.getConfig(JdbiImmutables.class).registerImmutable(Person.class);
+        IPersonDAO personDAO = new PersonDAO(jdbi);
+
+        environment.jersey().register(new PersonResource(personDAO));
         environment.healthChecks().register(this.getName(), new DropwizardJDBIExampleHealthCheck());
     }
 
